d8448ec43ee77051262fc6d2ff48cc4f67fd9074
DavidDev25 <you@example.com>
2024-09-16 16:42:59 +0200

    startet with render function atm not working. Will be fixed by tommorow

---
+++ scripts/templates/templates.js
@@ -0,0 +1,22 @@
+function init (){
+    renderDishes();
+}
+
+
+function renderDishes(){
+    let dishesRef =  document.getElementById("content");
+    dishesRef.innerHTML = "";
+
+    for (let index = 0; index < dishes.lengthM; index++){
+        let dish  = dishes[index];
+
+        let dishesItem = /*html*/`
+            <div class= dishes-content>
+                <h2>${dish.name}</h2>
+                <p>${dish.price}</p>
+                <p>${dish.description}</p>
+            </div>
+        `;
+        dishesRef.innerHTML += dishesItem;
+    }
+}
