e85d17d94b7a07f61db2a8adf9e4fc63d0d6af3e
DavidDev25 <you@example.com>
2024-09-17 22:11:57 +0200

    refactored the whole code, added basket, added button " add to basket" and added "x" button (without function)

--- scripts/templates/templates.js
+++ scripts/templates/templates.js
@@ -1,22 +1,39 @@
-function init (){
+function init() {
     renderDishes();
 }
 
+function renderDishes() {
+    const dishesRef = document.getElementById("content");
 
-function renderDishes(){
-    let dishesRef =  document.getElementById("content");
-    dishesRef.innerHTML = "";
-
-    for (let index = 0; index < dishes.length; index++){
-        let dish  = dishes[index];
-
-        let dishesItem = /*html*/`
-            <div class= dishes-content>
-                <h2>${dish.name}</h2>
-                <p>${dish.price}</p>
-                <p>${dish.description}</p>
+    for (let index = 0; index < dishes.length; index++) {
+        let dish = document.createElement("div");
+        dish.className = 'dish';
+        dish.innerHTML = `
+            <div class="dish-content">
+                <h2>${dishes[index].name}</h2>
+                <p class="description">${dishes[index].description}</p>
+                <p class="price">Preis: €${dishes[index].price.toFixed(2)}</p>
+                <button class="icon-button" onclick="addToBasket('${dishes[index].name}', ${dishes[index].price.toFixed(2)})">
+                    ${createIcons()}
+                </button>
             </div>
         `;
-        dishesRef.innerHTML += dishesItem;
+        dishesRef.appendChild(dish);
     }
+}
+
+function addToBasket(dishName, dishPrice) {
+    const basketRef = document.querySelector('.basket');
+    
+    let basketItem = document.createElement("div");
+    basketItem.className = 'basket-item';
+    basketItem.innerHTML = `
+        <p>${dishName}: €${dishPrice.toFixed(2)}</p>
+        <button onclick="removeFromBasket()">x</button>
+    `;
+    basketRef.appendChild(basketItem);
+}
+
+function removeFromBasket() {
+
 }
